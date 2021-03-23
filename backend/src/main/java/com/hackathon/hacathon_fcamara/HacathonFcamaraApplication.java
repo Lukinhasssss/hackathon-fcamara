b7099a6bd981010ccce484725e317821eb7e3163
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-22 22:33:25 -0300

    Projeto criado e H2 configurado

---
+++ backend/src/main/java/com/hackathon/hacathon_fcamara/HacathonFcamaraApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.hacathon_fcamara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacathonFcamaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacathonFcamaraApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hackathon/hacathon_fcamara/HacathonFcamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.hacathon_fcamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacathonFcamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
