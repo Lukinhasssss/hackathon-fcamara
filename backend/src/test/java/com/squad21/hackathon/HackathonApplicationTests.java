3af6a1aae22bf2275c34da2e846f3914b920cb93
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-03-25 13:39:16 -0300

    Projeto backend recriado e H2 configurado

---
+++ backend/src/test/java/com/squad21/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad21.hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
