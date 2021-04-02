830b62853224d01922f721e5a352a467df2117c4
williammarinho93 <56370582+williammarinho93@users.noreply.github.com>
2021-04-02 11:34:12 -0300

    projeto react criado

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Escola Solidaria</h1>
+    </div>
+  );
+}
+
+export default App;
