5e614cf05ef75b30b385a4bde52460ce57b31e2d
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-03 12:26:40 -0300

    Adicionada a pagina dos pontos de coleta

---
+++ frontend/src/core/components/PontoDeColeta/index.tsx
@@ -0,0 +1,19 @@
+import './styles.css'
+
+const PontoDeColeta = () => {
+  return (
+    <div className="ponto-de-coleta-container">
+      <div className="ponto-de-coleta-img-container">
+        <img src="https://diversa.org.br/wp-content/uploads/2020/04/Marcos-Santos_USP-Imagens_6-1024x704.jpg" alt="escola.nome"/>
+      </div>
+
+      <h2>Escola Pedro Alvarez Cabral</h2>
+      
+      <p>Barueri, São Paulo</p>
+      <p>Rua João Lopez</p>
+      <p>Nº 532</p>
+    </div>
+  )
+}
+
+export default PontoDeColeta
--- frontend/src/pages/PontosDeColeta/index.tsx
+++ frontend/src/pages/PontosDeColeta/index.tsx
@@ -1,8 +1,28 @@
+import BackButton from '../../core/components/BackButton'
+import Header from '../../core/components/Header'
+import PontoDeColeta from '../../core/components/PontoDeColeta'
 import './styles.css'
 
 const PontosDeColeta = () => {
   return (
-    <h1>Pontos de Coleta</h1>
+    <>
+      <Header>
+        <BackButton />
+      </Header>
+
+      <div className="coleta-container">
+        <div className="escolas-encontradas">
+          <span>3</span>
+          <p>escolas encontradas</p>
+        </div>
+
+        <div className="ponto-de-coleta">
+          <PontoDeColeta />
+          <PontoDeColeta />
+          <PontoDeColeta />
+        </div>
+      </div>
+    </>
   )
 }
 
