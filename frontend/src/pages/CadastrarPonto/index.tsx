06ff8eaf2d73f990f18c9449d209664cd31fc71e
Lucas Monteiro <lukasmonteir0@hotmail.com>
2021-04-03 16:29:01 -0300

    Pagina de cadastro adicionada

--- frontend/src/pages/CadastrarPonto/index.tsx
+++ frontend/src/pages/CadastrarPonto/index.tsx
@@ -1,8 +1,61 @@
+import BackButton from '../../core/components/BackButton'
+import Header from '../../core/components/Header'
 import './styles.css'
 
 const CadastrarPonto = () => {
   return (
-    <h1>Cadastrar Ponto de Coleta</h1>
+    <>
+      <Header>
+        <BackButton />
+      </Header>
+
+      <form className="cadastro-container">
+        <div className="cadastro-content">
+          <h1>Cadastre aqui o ponto de coleta</h1>
+
+          <h2>Dados do local</h2>
+
+          <span>Nome do local</span>
+          <input type="text" className="coleta-w100" />
+
+          <span>URL da imagem</span>
+          <input type="text" className="coleta-w100" />
+
+          <div className="coleta-column-2">
+            <div>
+              <span>Logradouro</span>
+              <input type="text" className="coleta-w100" />
+            </div>
+
+            <div className="coleta-column-number">
+              <span>Número</span>
+              <input type="text" className="coleta-w100" />
+            </div>
+          </div>
+
+          <span>Bairro</span>
+          <input type="text" className="coleta-w100" />
+
+          <div className="coleta-column-2">
+            <div>
+              <span>Cidade</span>
+              <input type="text" className="coleta-w100" />
+            </div>
+
+            <div className="coleta-column-estado">
+              <span>Estado</span>
+              <input type="text" className="coleta-w100" />
+            </div>
+          </div>
+
+          <div className="submit-button">
+            <button>
+              <p>Cadastrar ponto de coleta</p>
+            </button>
+          </div>
+        </div>
+      </form>
+    </>
   )
 }
 
